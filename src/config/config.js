c95025e31ebf57575c94cf716eaa6b1f11d6b6f1
Amaya Agha <amaya.agha@gmail.com>
2021-10-08 16:55:30 -0400

    feat: adds player character

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,8 @@
 export default {
   type: Phaser.AUTO,  // Specify the underlying browser rendering engine (AUTO, CANVAS, WEBGL)
                       // AUTO will attempt to use WEBGL, but if not available it'll default to CANVAS
-  width: 1920,   // Game width in pixels
-  height: 1080,  // Game height in pixels
+  width: 600,   // Game width in pixels
+  height: 600,  // Game height in pixels
   backgroundColor: '#000000',
   margin:0,
   parent: 'phaser',
