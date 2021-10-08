e0d603cb157401dd93f9cd7bcc2d6f74f8db12b2
Ashley Williams <ashleyswilliams01@gmail.com>
2021-10-08 14:32:16 -0600

    feat: logout button in Game clearing cookies and rendering login page

--- client/components/Game.js
+++ client/components/Game.js
@@ -1,14 +1,21 @@
-import React, { useEffect } from 'react'
-import Game from '../../src/index'
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import Game from "../../src/index";
+import { logout } from "../store";
 
 export const GameView = () => {
+  const dispatch = useDispatch();
+
   useEffect(() => {
     window.game = new Game();
-  })
+  });
   return (
     <div id="phaser">
       <h1>Hello From the Game Component</h1>
+      <Link to="/">
+        <button onClick={() => dispatch(logout())}>Logout</button>
+      </Link>
     </div>
-  )
-}
-
+  );
+};
