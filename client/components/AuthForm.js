13cef779e2826ea6a57ed5ecf944f35113a28bd7
Ashley Williams <ashleyswilliams01@gmail.com>
2021-10-10 12:20:37 -0600

    style: login/signup

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -11,6 +11,7 @@ import Snackbar from "@material-ui/core/Snackbar";
 import Alert from "@material-ui/lab/Alert";
 import Avatar from "@material-ui/core/Avatar";
 import Paper from "@material-ui/core/Paper";
+import Card from "@material-ui/core/Card";
 import { makeStyles } from "@material-ui/core";
 import LockedOutlinedIcon from "@material-ui/icons/LockOutlined";
 import { useHistory } from "react-router-dom";
@@ -26,6 +27,7 @@ const useStyles = makeStyles((theme) => ({
   },
   form: {
     height: "60vh",
+    top: "50%",
     width: 350,
     position: "absolute",
     top: "50%",
@@ -36,19 +38,35 @@ const useStyles = makeStyles((theme) => ({
     padding: 20
   },
   text: {
-    color: "#ffff"
-    // textTransform: "lowercase"
+    color: "#f5f3e6",
+    fontFamily: "Cinzel Decorative"
+  },
+  textfield: {
+    backgroundColor: "#872441",
+    marginBottom: 20
+  },
+  btn: {
+    fontFamily: "Cinzel Decorative",
+    backgroundColor: "#872441",
+    color: "#f5f3e6",
+    marginTop: 20,
+    "&:hover": {
+      backgroundColor: "#344a95"
+    }
+  },
+  signup: {
+    color: "#f5f3e6",
+    paddingTop: 20
   }
 }));
-
 
 const AuthForm = (props) => {
   const classes = useStyles();
 
   const { name, displayName, error } = props;
-  const [email, setEmail] = useState("cody@charm.com");
-  const [password, setPassword] = useState("123");
-  const [firstName, setFirstName] = useState("Cody");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
 
   const [snackBarWarningOpen, setSnackBarWarningOpen] = useState(false);
   const [snackBarErrorOpen, setSnackBarErrorOpen] = useState(false);
@@ -95,31 +113,97 @@ const AuthForm = (props) => {
   };
 
   return (
-    <Grid className={classes.bkimg}>
-    <Paper elevation={15} className={classes.form}>
-      <Grid align="center" className={classes.text}>
-        <Avatar style={{ backgroundColor: "#414dbf" }}>
-          <LockedOutlinedIcon />
-        </Avatar>
-        <h2>Login</h2>
-      </Grid>
-      <Box component="form" onSubmit={handleSubmit} name={name}>
-      <TextField
-        required
-        fullWidth
-        label="Email Address"
-        value={email}
-        onChange={(evt) => {
-          setEmail(evt.target.value);
-        }}
-        name="email"
-        type="text"
-      >
-
-      </TextField>
-      </Box>
-    </Paper>
-  </Grid>
+    <div className={classes.bkimg}>
+      <Snackbar
+        open={snackBarErrorOpen}
+        autoHideDuration={3000}
+        onClose={handleErrorClose}
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}>
+        <Alert onClose={handleErrorClose} severity="error" sx={{ width: "100%" }}>
+          This email address is already associated with an account
+        </Alert>
+      </Snackbar>
+      <Snackbar
+        open={snackBarWarningOpen}
+        autoHideDuration={3000}
+        onClose={handleWarningClose}
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}>
+        <Alert onClose={handleWarningClose} severity="warning" sx={{ width: "100%" }}>
+          Incorrect Email/Password
+        </Alert>
+      </Snackbar>
+      <Card elevation={15} className={classes.form}>
+        <Grid align="center" className={classes.text}>
+          <Avatar style={{ backgroundColor: "#344a95" }}>
+            <LockedOutlinedIcon />
+          </Avatar>
+          <h2>{displayName}</h2>
+        </Grid>
+        <Box component="form" onSubmit={handleSubmit} name={name}>
+          <TextField
+            required
+            fullWidth
+            label="Email Address"
+            value={email}
+            onChange={(evt) => {
+              setEmail(evt.target.value);
+            }}
+            name="email"
+            type="text"
+            className={classes.textfield}
+            InputLabelProps={{
+              className: classes.text
+            }}
+            variant="outlined"></TextField>
+          <TextField
+            required
+            fullWidth
+            label="Password"
+            value={password}
+            onChange={(evt) => {
+              setPassword(evt.target.value);
+            }}
+            name="password"
+            // styling goes to hell if I add this
+            // so currently, the password is not hidden when typing
+            // type="password"
+            className={classes.textfield}
+            InputLabelProps={{
+              className: classes.text
+            }}
+            variant="outlined"></TextField>
+          {name === "signup" && (
+            <TextField
+              required
+              fullWidth
+              label="First Name"
+              value={firstName}
+              onChange={(evt) => {
+                setFirstName(evt.target.value);
+              }}
+              name="firstName"
+              type="text"
+              className={classes.textfield}
+              InputLabelProps={{
+                className: classes.text
+              }}
+              variant="outlined"></TextField>
+          )}
+          <Grid align="center">
+            <Button type="submit" className={classes.btn}>
+              {displayName}
+            </Button>
+          </Grid>
+          {name === "login" && (
+            <Link to="signup">
+              <Grid align="center" className={classes.signup}>
+                <p>New player? Sign Up Here</p>
+              </Grid>
+            </Link>
+          )}
+        </Box>
+      </Card>
+    </div>
   );
 };
 
