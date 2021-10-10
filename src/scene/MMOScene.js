69468a52b32aab099243d0736db5ce71ab065041
Ava Choi <alexbchoi@gmail.com>
2021-10-10 17:19:46 -0400

    minimap

--- src/scene/MMOScene.js
+++ src/scene/MMOScene.js
@@ -1,27 +1,22 @@
-import 'phaser'
-import {Player} from '../entity/Player';
-import {PathGrid} from '../pathfinding/PathGrid';
+import "phaser";
+import { Player } from "../entity/Player";
+import { PathGrid } from "../pathfinding/PathGrid";
 
 export default class MMOScene extends Phaser.Scene {
   constructor() {
-    super('MMOScene');
+    super("MMOScene");
   }
 
-
-  preload() {
-
-
-  }
+  preload() {}
 
   create() {
-
     /**creating a map based on a tileset**/
-    const map = this.make.tilemap({key: 'start-scene'}) //the key: should match what you specified in this.load.tilemapTiledJSON
+    const map = this.make.tilemap({ key: "start-scene" }); //the key: should match what you specified in this.load.tilemapTiledJSON
     //tileSetName has to match the name of the tileset in Tiled, and the key is the image key we used for this tile set
-    const groundTiles = map.addTilesetImage('town', 'town'); //loads the tileset used to make up this map
-    this.groundLayer = map.createLayer('ground', groundTiles, 0, 0);
-    map.createLayer('world', groundTiles, 0, 0);
-    map.createLayer('belowChar', groundTiles, 0, 0);
+    const groundTiles = map.addTilesetImage("town", "town"); //loads the tileset used to make up this map
+    this.groundLayer = map.createLayer("ground", groundTiles, 0, 0);
+    map.createLayer("world", groundTiles, 0, 0);
+    map.createLayer("belowChar", groundTiles, 0, 0);
     // map.createLayer('house', groundTiles, 0, 0);
     // map.createLayer('Tile Layer 5', groundTiles, 0, 0);
     // map.createLayer('rocks', groundTiles, 0, 0);
@@ -29,15 +24,30 @@ export default class MMOScene extends Phaser.Scene {
     let grid = new PathGrid(this, 100, this.groundLayer.width);
     // groundLayer.setPipeline('Light2D');
 
-    this.player = new Player(this, grid, 0, 0, 'player', 'fox');
+    this.player = new Player(this, grid, 0, 0, "player", "fox");
     this.cameras.main.startFollow(this.player);
-
 
     // groundLayer.renderDebug(debugGraphics, {
     //   tileColor: null, // Color of non-colliding tiles
     //   collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
     //   faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
     // });
+
+    //  The miniCam is 400px wide, so can display the whole world at a zoom of 0.2
+    this.minimap = this.cameras
+      .add(795, 0, 230, 230)
+      .setZoom(0.3)
+      .setName("mini")
+      .startFollow(this.player);
+    this.minimap.setBackgroundColor(0x002244);
+
+    // this.minimap.scrollX = 820;
+    // this.minimap.scrollY = 700;
+    this.minimap.centerOn(0, 0);
+    const minimapCircle = new Phaser.GameObjects.Graphics(this);
+    minimapCircle.fillCircle(910, 115, 110);
+    const circle = new Phaser.Display.Masks.GeometryMask(this, minimapCircle);
+    this.minimap.setMask(circle, true);
   }
 
   /**anything that needs to update, should get it's update function called here**/
@@ -45,4 +55,3 @@ export default class MMOScene extends Phaser.Scene {
     this.player.update();
   }
 }
-
