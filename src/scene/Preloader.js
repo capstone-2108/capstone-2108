41fed0d3dcd5e323ce8739c2a42a87f0cadb9ba8
Ashley Williams <ashleyswilliams01@gmail.com>
2021-10-15 07:20:15 -0600

    chore: deletes duplicate code

--- src/scene/Preloader.js
+++ src/scene/Preloader.js
@@ -39,7 +39,6 @@ export default class Preloader extends Phaser.Scene {
     /**How to load a tile set**/
     this.load.image("town", "tilesets/tileset/RPG tileset (full) v1.5 - 200_.png");
     this.load.image("grass", "tilesets/forest/grass.png");
-    this.load.image("grass", "tilesets/forest/grass.png");
     this.load.image("plant", "tilesets/forest/plant.png");
     this.load.image("props", "tilesets/forest/props.png");
     this.load.image("stone", "tilesets/forest/stone.png");
