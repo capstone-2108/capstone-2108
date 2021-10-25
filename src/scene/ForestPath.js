14d4f189f7d5637dea3f9ab84a090fae38cd63ec
Ashley Williams <ashleyswilliams01@gmail.com>
2021-10-25 08:00:35 -0600

    adds scene music

--- src/scene/ForestPath.js
+++ src/scene/ForestPath.js
@@ -11,6 +11,9 @@ export default class ForestPath extends MMOScene {
   }
 
   create() {
+    const music = this.sound.add("scene3Audio");
+    music.play();
+
     const map = this.make.tilemap({ key: "forest-path" });
     const grassTiles = map.addTilesetImage("grass", "grass");
     const plantTiles = map.addTilesetImage("plant", "plant");
